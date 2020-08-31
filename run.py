3717a6900bc82b4796190dd71f7aeab3f0e442ec
Junho Yeo <hanaro0704@gmail.com>
2020-08-31 09:25:15 +0900

    feat: print timetable for today as table

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import datetime
 import json
 
+from rich.console import Console
+from rich.table import Column, Table
+
 def load_json_file(filename: str) -> dict:
   with open(filename) as file_stream:
     data = json.load(file_stream)
@@ -14,10 +17,22 @@ def get_classroom_from_subject_name(subject_name: str) -> dict:
     if subject_name == classroom['name']:
       return classroom
 
+def print_classroom(classroom: dict) -> None:
+  console = Console()
+
+  table = Table(show_header=True, header_style='bold magenta')
+  table.add_column('시간', style='dim', width=5)
+  table.add_column('과목명')
+  table.add_column('선생님', justify='right')
+
+  for classroom_index, classroom in enumerate(timetable_for_today):
+    table.add_row(f'{classroom_index + 1}교시', classroom['name'], classroom['teacher'])
+
+  console.print(table)
+
 weekday = datetime.datetime.today().weekday()
 timetable_for_today = [
   get_classroom_from_subject_name(subject_name)
   for subject_name in timetable[weekday]
 ]
-
-print(timetable_for_today)
+print_classroom(timetable_for_today)
